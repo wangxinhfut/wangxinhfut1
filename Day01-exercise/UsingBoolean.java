76fdabae7041c7ef3d8ddd6e57db9418b493ffe2
王鑫 <1027180025@qq.com>
2015-07-07 20:22:01 +0800

    作业
    作业

---
+++ Day01-exercise/UsingBoolean.java
@@ -0,0 +1,18 @@
+package wangxin;
+
+public class UsingBoolean {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+boolean a=false;
+boolean b=true;
+System.out.println(a);
+System.out.println(b);
+
+
+	}
+
+}
